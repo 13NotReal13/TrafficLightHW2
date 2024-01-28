cf3fc51d1998b742da3e059743b8fa3659453cd7
Ivan Semikin <i.semikin.94@icloud.com>
2024-01-28 13:46:08 +0100

    Logic of change color

--- TrafficLightHW2/ViewController.swift
+++ TrafficLightHW2/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redColorView: UIView!
+    @IBOutlet var yellowColorView: UIView!
+    @IBOutlet var greenColorView: UIView!
+    @IBOutlet var changeColorButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redColorView.layoutIfNeeded()
+        redColorView.layer.cornerRadius = min(
+            redColorView.bounds.width,
+            redColorView.bounds.height
+        ) / 2.0
+        yellowColorView.layoutIfNeeded()
+        yellowColorView.layer.cornerRadius = min(
+            yellowColorView.bounds.width,
+            yellowColorView.bounds.height
+        ) / 2.0
+        greenColorView.layoutIfNeeded()
+        greenColorView.layer.cornerRadius = min(
+            greenColorView.bounds.width,
+            greenColorView.bounds.height
+        ) / 2.0
+        changeColorButton.layer.cornerRadius = 10
     }
 
-
+    @IBAction func changeColorButtonDidTupped() {
+        if redColorView.alpha == 1 {
+            redColorView.alpha = 0.3
+            yellowColorView.alpha = 1
+        } else if yellowColorView.alpha == 1 {
+            yellowColorView.alpha = 0.3
+            greenColorView.alpha = 1
+        } else {
+            redColorView.alpha = 1
+            greenColorView.alpha = 0.3
+            changeColorButton.setTitle("NEXT", for: .normal)
+        }
+    }
+    
 }
 
