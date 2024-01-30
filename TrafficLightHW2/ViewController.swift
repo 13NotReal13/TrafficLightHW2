0ec76b056ae0c2d2c724718ceef188e26cb49efc
Ivan Semikin <i.semikin.94@icloud.com>
2024-01-30 10:09:23 +0100

    Clear project

--- TrafficLightHW2/ViewController.swift
+++ TrafficLightHW2/ViewController.swift
@@ -16,36 +16,11 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        redColorView.layoutIfNeeded()
-        redColorView.layer.cornerRadius = min(
-            redColorView.bounds.width,
-            redColorView.bounds.height
-        ) / 2.0
-        yellowColorView.layoutIfNeeded()
-        yellowColorView.layer.cornerRadius = min(
-            yellowColorView.bounds.width,
-            yellowColorView.bounds.height
-        ) / 2.0
-        greenColorView.layoutIfNeeded()
-        greenColorView.layer.cornerRadius = min(
-            greenColorView.bounds.width,
-            greenColorView.bounds.height
-        ) / 2.0
-        changeColorButton.layer.cornerRadius = 10
+  
     }
 
     @IBAction func changeColorButtonDidTupped() {
-        if redColorView.alpha == 1 {
-            redColorView.alpha = 0.3
-            yellowColorView.alpha = 1
-        } else if yellowColorView.alpha == 1 {
-            yellowColorView.alpha = 0.3
-            greenColorView.alpha = 1
-        } else {
-            redColorView.alpha = 1
-            greenColorView.alpha = 0.3
-            changeColorButton.setTitle("NEXT", for: .normal)
-        }
+  
     }
     
 }
