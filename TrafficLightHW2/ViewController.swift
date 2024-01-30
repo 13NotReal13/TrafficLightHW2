1a17e6b38082f89351ee4e2c402dfd684e970505
Ivan Semikin <i.semikin.94@icloud.com>
2024-01-30 10:13:34 +0100

    Setup constraints

--- TrafficLightHW2/ViewController.swift
+++ TrafficLightHW2/ViewController.swift
@@ -12,16 +12,54 @@ class ViewController: UIViewController {
     @IBOutlet var redColorView: UIView!
     @IBOutlet var yellowColorView: UIView!
     @IBOutlet var greenColorView: UIView!
-    @IBOutlet var changeColorButton: UIButton!
+    
+    @IBOutlet var startButton: UIButton!
+    
+    private var currentColor = currentColorEnum.red
+    private let lightIsOn: CGFloat = 1
+    private let lightIsOff: CGFloat = 0.3
     
     override func viewDidLoad() {
         super.viewDidLoad()
-  
+        startButton.layer.cornerRadius = 10
+        
+        redColorView.alpha = lightIsOff
+        yellowColorView.alpha = lightIsOff
+        greenColorView.alpha = lightIsOff
+    }
+    
+    override func viewWillLayoutSubviews() {
+        redColorView.layer.cornerRadius = redColorView.frame.width / 2
+        yellowColorView.layer.cornerRadius = yellowColorView.frame.width / 2
+        greenColorView.layer.cornerRadius = greenColorView.frame.width / 2
     }
 
     @IBAction func changeColorButtonDidTupped() {
-  
+        if startButton.currentTitle == "START" {
+            startButton.setTitle("NEXT", for: .normal)
+        }
+        
+        switch currentColor {
+        case .red:
+            greenColorView.alpha = lightIsOff
+            redColorView.alpha = lightIsOn
+            currentColor = .yellow
+        case .yellow:
+            redColorView.alpha = lightIsOff
+            yellowColorView.alpha = lightIsOn
+            currentColor = .green
+        case .green:
+            yellowColorView.alpha = lightIsOff
+            greenColorView.alpha = lightIsOn
+            currentColor = .red
+        }
     }
     
+}
+
+extension ViewController {
+    private enum currentColorEnum {
+        case red, yellow, green
+    }
 }
 
